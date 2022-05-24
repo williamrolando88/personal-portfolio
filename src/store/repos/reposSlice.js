85ddbc13db8128ad7b3a9d8a6380565b73a1b54d
williamrolando88 <william.rolando88@hotmail.com>
2022-05-23 22:26:44 -0500

    Add reposSlice connection to redux store

---
+++ src/store/repos/reposSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+const reposSlice = createSlice({
+  name: 'repos',
+  initialState,
+  reducers: {},
+  extraReducers: {},
+});
+
+export default reposSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
+import reposReducer from './repos/reposSlice';
 
-const reducer = {};
+const reducer = {
+  repos: reposReducer,
+};
 
 const store = configureStore({
   reducer,
