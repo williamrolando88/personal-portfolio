42790ba24428c4170fabf04c4deeb0c4d675653e
williamrolando88 <william.rolando88@hotmail.com>
2022-05-24 14:42:47 -0500

    Add style to `OtherProjects` component

--- src/components/OtherProjects.jsx
+++ src/components/OtherProjects.jsx
@@ -16,19 +16,23 @@ const OtherProjects = ({ project, onOpenModal }) => {
 
   return (
     <article className="flex flex-col border">
-      <img
-        className="h-full w-full object-contain object-top"
-        src={`https://raw.githubusercontent.com/williamrolando88/${name}/dev/screenshots/responsive_design.png`}
-        alt={`Snapshot for ${formatTitle(name)}`}
-      />
-      <h3 className="text-2xl font-serif">{formatTitle(name)}</h3>
-      <p>{trimString(description, 100)}</p>
-      <div className="flex flex-wrap gap-2">
-        {topics.map((topic) => (
-          <span key={topic} className="px-3 py-2 bg-white/30">
-            {topic}
-          </span>
-        ))}
+      <div className="flex grow flex-col gap-2 bg-gradient-to-b from-transparent text-white to-black ">
+        <img
+          className="h-full w-full object-contain object-top"
+          src={`https://raw.githubusercontent.com/williamrolando88/${name}/dev/screenshots/responsive_design.png`}
+          alt={`Snapshot for ${formatTitle(name)}`}
+        />
+        <div className="flex gap-2 flex-col px-4 py-2">
+          <h3 className="text-2xl font-serif">{formatTitle(name)}</h3>
+          <p>{trimString(description, 80)}</p>
+          <div className="flex flex-wrap gap-2">
+            {topics.map((topic) => (
+              <span key={topic} className="px-2 py-1 text-sm bg-white/30">
+                {topic}
+              </span>
+            ))}
+          </div>
+        </div>
       </div>
       <button onClick={() => onOpenModal(index)} className="btn-primary w-full">
         See Project
