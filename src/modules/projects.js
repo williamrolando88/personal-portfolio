5cf91f870a40164d2c558150e9e29e061360dd10
williamrolando88 <william.rolando88@hotmail.com>
2022-05-23 22:26:59 -0500

    Add projects info

---
+++ src/modules/projects.js
@@ -0,0 +1,50 @@
+export const projects = [
+  {
+    name: 'Hotelzilla-UI',
+    featured: true,
+  },
+  {
+    name: 'space-travelers-hub',
+    featured: false,
+  },
+  {
+    name: 'es-tournament',
+    featured: false,
+  },
+  {
+    name: 'world-covid-tracker',
+    featured: false,
+  },
+  {
+    name: 'Budget-app',
+    featured: false,
+  },
+  {
+    name: 'scientech-cost-calculator',
+    featured: false,
+  },
+  {
+    name: 'ultimate-show-guide',
+    featured: false,
+  },
+  {
+    name: 'bookstore',
+    featured: false,
+  },
+  {
+    name: 'leaderboard',
+    featured: false,
+  },
+  {
+    name: 'math-magician',
+    featured: false,
+  },
+  {
+    name: 'My-catalog',
+    featured: false,
+  },
+  {
+    name: 'Recipe-app',
+    featured: false,
+  },
+];
