6af28700a25481a518b4f16173b7340007f23f33
williamrolando88 <william.rolando88@hotmail.com>
2022-05-24 14:17:26 -0500

    Add content to `OtherProjects` component

--- src/components/OtherProjects.jsx
+++ src/components/OtherProjects.jsx
@@ -1,36 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { formatTitle, trimString } from '../modules/formatString';
 
-const OtherProjects = (props) => {
-  const {
-    project: { title, thumbnail, shortDescription, technologies, index },
-    onOpenModal,
-  } = props;
+const OtherProjects = ({ project, onOpenModal }) => {
+  const count = useRef(0);
+  useEffect(() => {
+    count.current = count.current + 1;
+  }, []);
+
+  const { name, description, topics, index } = project;
+
+  console.log(count.current, name);
 
   return (
-    <article className="flex flex-col">
+    <article className="flex flex-col border">
       <img
         className="h-full w-full object-contain object-top"
-        src={thumbnail}
-        alt={`Snapshot for ${title}`}
+        src={`https://raw.githubusercontent.com/williamrolando88/${name}/dev/screenshots/responsive_design.png`}
+        alt={`Snapshot for ${formatTitle(name)}`}
       />
-      <div className="-mt-52">
-        <div className="flex flex-col gap-3 justify-end p-4 bg-gradient-to-b from-transparent via-black to-black text-white">
-          <h3 className="text-2xl font-serif">{title}</h3>
-          <p>{shortDescription}</p>
-          <div className="flex flex-wrap gap-4">
-            {technologies.map((technology, index) => (
-              <a key={index} className="px-3 py-2 bg-white/30">
-                {technology}
-              </a>
-            ))}
-          </div>
-        </div>
-        <button
-          onClick={() => onOpenModal(index)}
-          className="btn-primary w-full">
-          See Project
-        </button>
+      <h3 className="text-2xl font-serif">{formatTitle(name)}</h3>
+      <p>{trimString(description, 100)}</p>
+      <div className="flex flex-wrap gap-2">
+        {topics.map((topic) => (
+          <span key={topic} className="px-3 py-2 bg-white/30">
+            {topic}
+          </span>
+        ))}
       </div>
+      <button onClick={() => onOpenModal(index)} className="btn-primary w-full">
+        See Project
+      </button>
     </article>
   );
 };
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import FeaturedProject from './FeaturedProject';
 import OtherProjects from './OtherProjects';
 import ProjectModal from './ProjectModal';
@@ -8,17 +9,7 @@ const Portfolio = () => {
   const [modalOpen, setModalOpen] = useState(false);
   const [modalProject, setModalProject] = useState({});
 
-  useEffect(() => {
-    const getProjects = async () => {
-      const response = await fetch('./json/projects.json');
-      const data = await response.json();
-      setProjects(data);
-    };
-    getProjects();
-    return () => {
-      setProjects([]);
-    };
-  }, []);
+  const { featuredRepo, reposInfo } = useSelector((store) => store.repos);
 
   const handleOpenModal = (index) => {
     setModalOpen(true);
@@ -29,7 +20,8 @@ const Portfolio = () => {
     <div className="w-full">
       <section
         id="projects"
-        className="bg-slate-50 w-full px-6 py-28 md:px-[10%]">
+        className="bg-slate-50 w-full px-6 py-28 md:px-[10%]"
+      >
         <div>
           <div className="flex flex-col justify-center md:flex-row md:items-center md:gap-6">
             <h2 className="text-3xl md:text-4xl font-serif self-center min-w-fit">
@@ -38,42 +30,34 @@ const Portfolio = () => {
             <div className="h-px w-full bg-black my-6" />
           </div>
           <div>
-            {projects.map((project, index) => {
-              if (project.featured) {
-                return (
-                  <FeaturedProject
-                    key={index}
-                    project={project}
-                    onOpenModal={handleOpenModal}
-                  />
-                );
-              }
-            })}
+            {Object.keys(featuredRepo).length > 0 && (
+              <FeaturedProject
+                key={featuredRepo.id}
+                project={featuredRepo}
+                onOpenModal={handleOpenModal}
+              />
+            )}
 
-            <div className="mt-6 grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
-              {projects.map((project, index) => {
-                if (!project.featured) {
-                  return (
-                    <OtherProjects
-                      key={index}
-                      project={project}
-                      onOpenModal={handleOpenModal}
-                    />
-                  );
-                }
-              })}
+            <div className="mt-6 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+              {Object.keys(reposInfo).map((repo) => (
+                <OtherProjects
+                  key={reposInfo[repo].name}
+                  project={reposInfo[repo]}
+                  onOpenModal={handleOpenModal}
+                />
+              ))}
             </div>
           </div>
         </div>
       </section>
-      {modalOpen && (
+      {/* {modalOpen && (
         <div className="block fixed top-0 left-0 z-10 w-full">
           <ProjectModal
             project={modalProject}
             onCloseModal={() => setModalOpen(false)}
           />
         </div>
-      )}
+      )} */}
     </div>
   );
 };
